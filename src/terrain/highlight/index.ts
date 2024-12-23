2a881c9736d089c3aa8284fcbaa512b50049765f
wang-muhan <wangmuhan.official@gmail.com>
2024-12-23 13:35:20 +0800

    add highlight

--- src/terrain/highlight/index.ts
+++ src/terrain/highlight/index.ts
@@ -1,14 +1,14 @@
 import * as THREE from 'three'
-import Terrain from '..'
+import Terrain, {BlockType, WorldType} from '..'
 
 /**
  * Highlight block on crosshair
  */
 export default class BlockHighlight {
   constructor(
-    scene: THREE.Scene,
-    camera: THREE.PerspectiveCamera,
-    terrain: Terrain
+      scene: THREE.Scene,
+      camera: THREE.PerspectiveCamera,
+      terrain: Terrain
   ) {
     this.camera = camera
     this.scene = scene
@@ -30,14 +30,19 @@ export default class BlockHighlight {
     opacity: 0.25
     // depthWrite: false
   })
+  instanceMaterial = new THREE.MeshStandardMaterial({
+    transparent: true,
+    opacity: 0.5,
+    color: new THREE.Color(0, 1, 0)
+  })
   mesh = new THREE.Mesh(new THREE.BoxGeometry(), this.material)
 
   // block simulation
   index = 0
   instanceMesh = new THREE.InstancedMesh(
-    new THREE.BoxGeometry(),
-    new THREE.MeshBasicMaterial(),
-    1000
+      new THREE.BoxGeometry(),
+      new THREE.MeshBasicMaterial(),
+      1500
   )
 
   update() {
@@ -45,8 +50,8 @@ export default class BlockHighlight {
     this.scene.remove(this.mesh)
     this.index = 0
     this.instanceMesh.instanceMatrix = new THREE.InstancedBufferAttribute(
-      new Float32Array(1000 * 16),
-      16
+        new Float32Array(1000 * 16),
+        16
     )
 
     const position = this.camera.position
@@ -63,9 +68,9 @@ export default class BlockHighlight {
         let x = xPos + i
         let z = zPos + j
         let y =
-          Math.floor(
-            noise.get(x / noise.gap, z / noise.gap, noise.seed) * noise.amp
-          ) + 30
+            Math.floor(
+                noise.get(x / noise.gap, z / noise.gap, noise.seed) * noise.amp
+            ) + 30
 
         idMap.set(`${x}_${y}_${z}`, this.index)
         matrix.setPosition(x, y, z)
@@ -79,46 +84,157 @@ export default class BlockHighlight {
             noise.get(x / noise.treeGap, z / noise.treeGap, noise.treeSeed) *
             noise.treeAmp
 
-        // check tree
-        if (
-            treeOffset > noise.treeThreshold &&
-            y - 30 >= -3 &&
-            stoneOffset < noise.stoneThreshold
-        ) {
-          for (let t = 1; t <= noise.treeHeight; t++) {
-            idMap.set(`${x}_${y + t}_${z}`, this.index)
-            matrix.setPosition(x, y + t, z)
-            this.instanceMesh.setMatrixAt(this.index++, matrix)
+        if (this.terrain.worldtype === WorldType.overworld) {
+          // check tree
+          if (
+              treeOffset > noise.treeThreshold &&
+              y - 30 >= -3 &&
+              stoneOffset < noise.stoneThreshold
+          ) {
+            for (let t = 1; t <= noise.treeHeight; t++) {
+              idMap.set(`${x}_${y + t}_${z}`, this.index)
+              matrix.setPosition(x, y + t, z)
+              this.instanceMesh.setMatrixAt(this.index++, matrix)
+            }
+
+            // leaf
+            // for (let i = -3; i < 3; i++) {
+            //   for (let j = -3; j < 3; j++) {
+            //     for (let k = -3; k < 3; k++) {
+            //       if (i === 0 && k === 0) {
+            //         continue
+            //       }
+            //     let leafOffset =
+            //       noise.get(
+            //         (x + i + j) / noise.leafGap,
+            //         (z + k) / noise.leafGap,
+            //         noise.leafSeed
+            //       ) * noise.leafAmp
+
+            //       if (leafOffset > noise.leafThreshold) {
+            //         idMap.set(
+            //           `${x + i}_${y + noise.treeHeight + j}_${z + k}`,
+            //           this.index
+            //         )
+            //         matrix.setPosition(x + i, y + noise.treeHeight + j, z + k)
+            //         this.instanceMesh.setMatrixAt(this.index++, matrix)
+            //       }
+            //     }
+            //   }
+            // }
+          }
+        } else if (this.terrain.worldtype === WorldType.nether) {
+          y = 30
+          // generate ceilings
+          const ceilingmean = Math.floor(
+              noise.get(x / noise.netherGap, z / noise.netherGap, noise.netherSeed) * noise.netherAmp + 10
+          )
+          const ceilingstd = 1
+          for (let yOffset = ceilingmean - ceilingstd; yOffset <= ceilingmean - ceilingstd; yOffset++) {
+            matrix.setPosition(x, y + yOffset, z)
+
+            const stoneOffset =
+                noise.get(x / noise.stoneGap, z / noise.stoneGap, noise.stoneSeed) *
+                noise.stoneAmp
+
+            const coalOffset =
+                noise.get(x / noise.coalGap, z / noise.coalGap, noise.coalSeed) *
+                noise.coalAmp
+
+            // set stones and coal
+            if (stoneOffset > noise.stoneThreshold) {
+              if (coalOffset > noise.coalThreshold) {
+                // coal
+                idMap.set(`${x}_${y + yOffset}_${z}`, this.index)
+                this.instanceMesh.setMatrixAt(
+                    this.index++,
+                    matrix
+                )
+              } else {
+                // stone
+                idMap.set(`${x}_${y + yOffset}_${z}`, this.index)
+                this.instanceMesh.setMatrixAt(
+                    this.index++,
+                    matrix
+                )
+              }
+            }
+
+            // set grass, sand and water
+            else {
+              if (yOffset < -3) {
+                // sand
+                idMap.set(`${x}_${y + yOffset}_${z}`, this.index)
+                this.instanceMesh.setMatrixAt(
+                    this.index++,
+                    matrix
+                )
+              } else {
+                // nether_bricks
+                idMap.set(`${x}_${y + yOffset}_${z}`, this.index)
+                this.instanceMesh.setMatrixAt(
+                    this.index++,
+                    matrix
+                )
+              }
+            }
           }
 
-          // leaf
-          // for (let i = -3; i < 3; i++) {
-          //   for (let j = -3; j < 3; j++) {
-          //     for (let k = -3; k < 3; k++) {
-          //       if (i === 0 && k === 0) {
-          //         continue
-          //       }
-          //     let leafOffset =
-          //       noise.get(
-          //         (x + i + j) / noise.leafGap,
-          //         (z + k) / noise.leafGap,
-          //         noise.leafSeed
-          //       ) * noise.leafAmp
+          // generate magma
+          const yOffset = y - 30
+          if (
+              treeOffset > noise.magmaThreshold &&
+              yOffset >= -3 && // not in water or sand
+              stoneOffset < noise.stoneThreshold // not on stones
+          ) {
+            for (let i = 1; i <= noise.magmaHeight; i++) {
+              idMap.set(`${x}_${y + yOffset + i}_${z}`, this.index)
 
-          //       if (leafOffset > noise.leafThreshold) {
-          //         idMap.set(
-          //           `${x + i}_${y + noise.treeHeight + j}_${z + k}`,
-          //           this.index
-          //         )
-          //         matrix.setPosition(x + i, y + noise.treeHeight + j, z + k)
-          //         this.instanceMesh.setMatrixAt(this.index++, matrix)
-          //       }
-          //     }
-          //   }
-          // }
+              matrix.setPosition(x, y + yOffset + i, z)
+
+              this.instanceMesh.setMatrixAt(
+                  this.index++,
+                  matrix
+              )
+            }
+
+            for (let j = -3; j < 3; j++) {
+              let l = (j + 5) / 2
+              for (let i = -l; i < l; i++) {
+                for (let k = -l; k < l; k++) {
+                  if (i === 0 && k === 0) {
+                    continue
+                  }
+                  const leafOffset =
+                      noise.get(
+                          (x + i + j) / noise.leafGap,
+                          (z + k) / noise.leafGap,
+                          noise.leafSeed
+                      ) * noise.leafAmp
+
+                  // add leaf if noise is greater than threshold
+                  if (leafOffset > noise.leafThreshold) {
+
+                    idMap.set(`${x}_${y + yOffset + noise.magmaHeight + j}_${z}`, this.index)
+
+                    matrix.setPosition(
+                        x + i,
+                        y + yOffset + noise.magmaHeight + j,
+                        z + k
+                    )
+                    this.instanceMesh.setMatrixAt(
+                        this.index++,
+                        matrix
+                    )
+                  }
+                }
+              }
+            }
+          }
         }
       }
     }
+
 
     // check custom blocks
     for (const block of this.terrain.customBlocks) {
@@ -129,25 +245,25 @@ export default class BlockHighlight {
         if (idMap.has(`${block.x}_${block.y}_${block.z}`)) {
           let id = idMap.get(`${block.x}_${block.y}_${block.z}`)
           this.instanceMesh.setMatrixAt(
-            id!,
-            new THREE.Matrix4().set(
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0,
-              0
-            )
+              id!,
+              new THREE.Matrix4().set(
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0,
+                  0
+              )
           )
         }
       }
@@ -157,9 +273,9 @@ export default class BlockHighlight {
     this.raycaster.setFromCamera({ x: 0, y: 0 }, this.camera)
     this.block = this.raycaster.intersectObject(this.instanceMesh)[0]
     if (
-      this.block &&
-      this.block.object instanceof THREE.InstancedMesh &&
-      typeof this.block.instanceId === 'number'
+        this.block &&
+        this.block.object instanceof THREE.InstancedMesh &&
+        typeof this.block.instanceId === 'number'
     ) {
       this.mesh = new THREE.Mesh(this.geometry, this.material)
       let matrix = new THREE.Matrix4()
@@ -169,5 +285,6 @@ export default class BlockHighlight {
       this.mesh.position.set(position.x, position.y, position.z)
       this.scene.add(this.mesh)
     }
+    console.log(this.index)
   }
 }
